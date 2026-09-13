a26c74459261981bc1c6bb2eb26b29657018b8d7
Tacitus37 <lunaris37@naver.com>
2026-09-13 19:07:20 +0900

    Add splash.jsx component for prototype

---
+++ move_agent_to_root/figma/prototype/a/splash.jsx
@@ -0,0 +1,34 @@
+<div className="w-96 h-[852px] relative bg-color-bg-subtle inline-flex flex-col justify-start items-start overflow-hidden">
+  <div className="w-96 h-14 px-6 bg-color-bg-subtle inline-flex justify-start items-center overflow-hidden">
+    <div className="flex-1 justify-start text-color-text-primary text-xs font-medium font-['Noto_Sans_KR'] leading-4">9:41</div>
+    <div className="flex-1 text-right justify-start text-color-text-primary text-xs font-medium font-['Noto_Sans_KR'] leading-4">▰</div>
+  </div>
+  <div className="w-96 px-6 flex flex-col justify-start items-start overflow-hidden">
+    <div className="self-stretch h-11 inline-flex justify-start items-center gap-2">
+      <div className="flex-1 justify-start text-color-text-primary text-base font-medium font-['Noto_Sans_KR'] leading-6">DOIT</div>
+      <div className="size-6 inline-flex flex-col justify-start items-start gap-2">
+        <div className="size-6 relative overflow-hidden">
+          <div className="size-4 left-[4px] top-[3px] absolute outline outline-2 outline-offset-[-0.90px] outline-slate-600" />
+        </div>
+      </div>
+    </div>
+  </div>
+  <div className="w-96 flex-1 px-6 pt-3 pb-6 flex flex-col justify-start items-start gap-4 overflow-hidden">
+    <div className="self-stretch justify-start text-color-brand-primary text-xs font-medium font-['Noto_Sans_KR'] leading-4">YOUR NEXT CHAPTER</div>
+    <div className="self-stretch justify-start text-color-text-primary text-xl font-bold font-['Noto_Sans_KR'] leading-8">군 복무의 오늘을,<br/>전역 후의 경쟁력으로.</div>
+    <div className="w-80 p-6 bg-color-brand-primary rounded-3xl flex flex-col justify-start items-start gap-6 overflow-hidden">
+      <div className="self-stretch justify-start text-color-text-on-primary text-3xl font-bold font-['Noto_Sans_KR'] leading-10">DOIT</div>
+      <div className="self-stretch justify-start text-color-text-on-primary text-2xl font-bold font-['Noto_Sans_KR'] leading-9">전역 후의 나에게,<br/>오늘의 작은 실행을.</div>
+      <div className="self-stretch justify-start text-color-text-on-primary text-xs font-medium font-['Noto_Sans_KR'] leading-4">PLAN / CONNECT / DO</div>
+    </div>
+    <div className="self-stretch justify-start text-color-text-secondary text-base font-normal font-['Noto_Sans_KR'] leading-6">목표부터 교재, 혜택, 오늘 할 일까지.<br/>남은 군생활을 하나의 계획으로 연결해요.</div>
+  </div>
+  <div className="w-96 px-6 pt-3 pb-14 bg-color-bg-subtle flex flex-col justify-start items-start gap-2 overflow-hidden">
+    <div data-icon="None" data-size="L" data-state="Default" data-variant="Primary" className="self-stretch h-14 relative rounded-2xl inline-flex justify-center items-center gap-2">
+      <div className="w-80 h-14 left-0 top-0 absolute bg-color-brand-primary rounded-2xl" />
+      <div className="flex-1 text-center justify-start text-color-text-on-primary text-base font-medium font-['Noto_Sans_KR'] leading-6">Label</div>
+    </div>
+  </div>
+  <div className="w-32 h-[5px] left-[129.50px] top-[830px] absolute bg-color-text-primary rounded-[999px]" />
+  <div className="w-28 h-8 left-[140px] top-[12px] absolute bg-black rounded-[999px]" />
+</div>
