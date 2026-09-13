4b053314e8205bea5b541f0993c80e854285ef18
Tacitus37 <lunaris37@naver.com>
2026-09-13 19:06:36 +0900

    Add Face ID authentication screen component

---
+++ move_agent_to_root/figma/screen/auth/face-id.jsx
@@ -0,0 +1,50 @@
+<div className="w-96 h-[852px] relative bg-color-bg-default inline-flex flex-col justify-start items-start overflow-hidden">
+  <div className="self-stretch h-14 px-6 relative bg-color-bg-default inline-flex justify-between items-center">
+    <div className="w-12 h-5 text-center justify-start text-color-system-black text-xs font-medium font-['Noto_Sans_KR'] leading-4">9:41</div>
+    <div className="w-16 h-5 relative overflow-hidden">
+      <div className="size-3 left-[2px] top-[3px] absolute bg-black outline outline-[2.80px] outline-offset-[-1.40px] outline-black" />
+      <div className="w-3.5 h-2 left-[24px] top-[4px] absolute outline outline-[1.60px] outline-offset-[-0.80px] outline-black" />
+      <div className="w-5 h-2.5 left-[45px] top-[5px] absolute outline outline-[1.40px] outline-offset-[-0.70px] outline-black" />
+      <div className="w-3.5 h-1.5 left-[47px] top-[7px] absolute bg-black" />
+      <div className="w-0 h-[3px] left-[66px] top-[9px] absolute bg-black outline outline-[1.50px] outline-offset-[-0.75px] outline-black" />
+    </div>
+    <div className="w-28 h-8 left-[139.50px] top-[12px] absolute bg-black rounded-[999px]" />
+  </div>
+  <div className="self-stretch px-6 py-2 flex flex-col justify-start items-start">
+    <div data-type="Subscreen" className="self-stretch h-11 inline-flex justify-start items-center gap-2">
+      <div className="w-28 h-11 min-w-11 min-h-11 flex justify-center items-center">
+        <div className="size-6 inline-flex flex-col justify-start items-start gap-2">
+          <div className="size-6 relative overflow-hidden">
+            <div className="w-1.5 h-3.5 left-[8px] top-[5px] absolute outline outline-2 outline-offset-[-0.90px] outline-slate-600" />
+          </div>
+        </div>
+      </div>
+      <div className="flex-1 justify-start text-color-text-primary text-lg font-medium font-['Noto_Sans_KR'] leading-6">생체 인증 설정</div>
+      <div className="size-11 relative" />
+    </div>
+  </div>
+  <div className="self-stretch flex-1 flex flex-col justify-start items-center overflow-hidden">
+    <div className="self-stretch px-6 pt-10 pb-8 flex flex-col justify-start items-center gap-12">
+      <div className="size-36 bg-color-brand-primary-soft rounded-3xl inline-flex justify-center items-center">
+        <div className="size-16 relative overflow-hidden">
+          <div className="size-14 left-[5.33px] top-[5.33px] absolute outline outline-2 outline-offset-[-1px] outline-color-brand-primary" />
+        </div>
+      </div>
+      <div className="self-stretch flex flex-col justify-start items-center gap-3">
+        <div className="self-stretch text-center justify-start text-color-text-primary text-2xl font-bold font-['Noto_Sans_KR'] leading-9">얼굴로 빠르게 로그인</div>
+        <div className="self-stretch text-center justify-start text-color-text-secondary text-base font-normal font-['Noto_Sans_KR'] leading-6">Face ID로 다음부터 더 빠르게<br/>접속할 수 있어요.</div>
+      </div>
+    </div>
+  </div>
+  <div className="self-stretch px-6 pt-5 pb-14 bg-color-bg-subtle flex flex-col justify-start items-center gap-4">
+    <div data-icon="None" data-size="L" data-state="Default" data-variant="Primary" className="self-stretch h-14 relative rounded-2xl inline-flex justify-center items-center gap-2">
+      <div className="w-80 h-14 left-0 top-0 absolute bg-color-brand-primary rounded-2xl" />
+      <div className="flex-1 text-center justify-start text-color-text-on-primary text-base font-medium font-['Noto_Sans_KR'] leading-6">Face ID 사용하기</div>
+    </div>
+    <div data-icon="None" data-size="L" data-state="Default" data-variant="Primary" className="self-stretch h-14 relative rounded-2xl inline-flex justify-center items-center gap-2">
+      <div className="w-80 h-14 left-0 top-0 absolute bg-color-brand-primary rounded-2xl" />
+      <div className="flex-1 text-center justify-start text-color-text-secondary text-base font-medium font-['Noto_Sans_KR'] leading-6">Label</div>
+    </div>
+  </div>
+  <div className="w-32 h-[5px] left-[129.50px] top-[830px] absolute bg-color-text-primary rounded-[999px]" />
+</div>
