d15a914f9d7927ea4b76bcb3ce3059ddf1bcc0ae
Tacitus37 <lunaris37@naver.com>
2026-09-13 19:04:28 +0900

    Add splash screen component for authentication

---
+++ move_agent_to_root/figma/screen/auth/splash.jsx
@@ -0,0 +1,30 @@
+<div className="w-96 h-[852px] relative bg-color-brand-primary inline-flex flex-col justify-start items-start overflow-hidden">
+  <div className="self-stretch h-14 px-6 relative bg-black/0 inline-flex justify-between items-center">
+    <div className="w-12 h-5 text-center justify-start text-color-bg-default text-xs font-medium font-['Noto_Sans_KR'] leading-4">9:41</div>
+    <div className="w-16 h-5 relative overflow-hidden">
+      <div className="size-3 left-[2px] top-[3px] absolute bg-color-bg-default outline outline-[2.80px] outline-offset-[-1.40px] outline-color-bg-default" />
+      <div className="w-3.5 h-2 left-[24px] top-[4px] absolute outline outline-[1.60px] outline-offset-[-0.80px] outline-color-bg-default" />
+      <div className="w-5 h-2.5 left-[45px] top-[5px] absolute outline outline-[1.40px] outline-offset-[-0.70px] outline-color-bg-default" />
+      <div className="w-3.5 h-1.5 left-[47px] top-[7px] absolute bg-color-bg-default" />
+      <div className="w-0 h-[3px] left-[66px] top-[9px] absolute bg-color-bg-default outline outline-[1.50px] outline-offset-[-0.75px] outline-color-bg-default" />
+    </div>
+    <div className="w-28 h-8 left-[139.50px] top-[12px] absolute bg-black rounded-[999px]" />
+  </div>
+  <div className="self-stretch flex-1 flex flex-col justify-center items-center gap-6">
+    <div className="size- flex flex-col justify-start items-center gap-4">
+      <div className="size-20 bg-color-bg-default rounded-3xl inline-flex justify-center items-center">
+        <div className="justify-start text-color-brand-primary text-5xl font-bold font-['Noto_Sans_KR']">D</div>
+      </div>
+      <div className="justify-start text-color-bg-default text-4xl font-bold font-['Noto_Sans_KR'] leading-10">DOIT</div>
+    </div>
+    <div className="opacity-80 justify-start text-color-brand-primary-soft text-sm font-normal font-['Noto_Sans_KR'] leading-5">PLAN / CONNECT / DO</div>
+  </div>
+  <div className="self-stretch pb-16 flex flex-col justify-start items-center gap-4">
+    <div className="size-6 flex flex-col justify-center items-center">
+      <div className="size-5 relative overflow-hidden">
+        <div className="size-4 left-[1.67px] top-[1.67px] absolute outline outline-2 outline-offset-[-1px] outline-color-bg-default" />
+      </div>
+    </div>
+  </div>
+  <div className="w-32 h-[5px] left-[129.50px] top-[830px] absolute bg-color-bg-default rounded-[999px]" />
+</div>
