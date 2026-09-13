0fd1feafd07f60e2991b1f1b8ac9d943a45c591b
Tacitus37 <lunaris37@naver.com>
2026-09-13 19:07:58 +0900

    Add login component in login.jsx

---
+++ move_agent_to_root/figma/prototype/a/login.jsx
@@ -0,0 +1,45 @@
+<div className="w-96 h-[852px] relative bg-color-bg-subtle inline-flex flex-col justify-start items-start overflow-hidden">
+  <div className="w-96 h-14 px-6 bg-color-bg-subtle inline-flex justify-start items-center overflow-hidden">
+    <div className="flex-1 justify-start text-color-text-primary text-xs font-medium font-['Noto_Sans_KR'] leading-4">9:41</div>
+    <div className="flex-1 text-right justify-start text-color-text-primary text-xs font-medium font-['Noto_Sans_KR'] leading-4">▰</div>
+  </div>
+  <div className="w-96 px-6 flex flex-col justify-start items-start overflow-hidden">
+    <div className="self-stretch h-11 inline-flex justify-start items-center gap-2">
+      <div className="size-6 inline-flex flex-col justify-start items-start gap-2">
+        <div className="size-6 relative overflow-hidden">
+          <div className="w-1.5 h-3.5 left-[8px] top-[5px] absolute outline outline-2 outline-offset-[-0.90px] outline-slate-600" />
+        </div>
+      </div>
+      <div className="flex-1 justify-start text-color-text-primary text-base font-medium font-['Noto_Sans_KR'] leading-6">DOIT</div>
+      <div className="size-6 inline-flex flex-col justify-start items-start gap-2">
+        <div className="size-6 relative overflow-hidden">
+          <div className="size-4 left-[4px] top-[3px] absolute outline outline-2 outline-offset-[-0.90px] outline-slate-600" />
+        </div>
+      </div>
+    </div>
+  </div>
+  <div className="w-96 flex-1 px-6 pt-3 pb-6 flex flex-col justify-start items-start gap-4 overflow-hidden">
+    <div className="self-stretch justify-start text-color-brand-primary text-xs font-medium font-['Noto_Sans_KR'] leading-4">WELCOME BACK</div>
+    <div className="self-stretch justify-start text-color-text-primary text-xl font-bold font-['Noto_Sans_KR'] leading-8">다시 만나 반가워요</div>
+    <div data-state="Filled" className="self-stretch px-4 py-3 bg-color-bg-subtle rounded-xl outline outline-1 outline-offset-[-1px] outline-color-border-default flex flex-col justify-start items-start gap-2">
+      <div className="self-stretch justify-start text-color-text-secondary text-xs font-medium font-['Noto_Sans_KR'] leading-4">이메일</div>
+      <div className="self-stretch justify-start text-color-text-secondary text-base font-normal font-['Noto_Sans_KR'] leading-6">demo@example.com</div>
+    </div>
+    <div data-state="Filled" className="self-stretch px-4 py-3 bg-color-bg-subtle rounded-xl outline outline-1 outline-offset-[-1px] outline-color-border-default flex flex-col justify-start items-start gap-2">
+      <div className="self-stretch justify-start text-color-text-secondary text-xs font-medium font-['Noto_Sans_KR'] leading-4">비밀번호</div>
+      <div className="self-stretch justify-start text-color-text-secondary text-base font-normal font-['Noto_Sans_KR'] leading-6">••••••••</div>
+    </div>
+    <div className="self-stretch justify-start text-color-text-secondary text-base font-normal font-['Noto_Sans_KR'] leading-6">DEMO 계정으로 체험할 수 있어요.</div>
+    <div data-icon="None" data-size="L" data-state="Default" data-variant="Secondary" className="self-stretch h-14 bg-color-brand-primary-soft rounded-2xl inline-flex justify-center items-center gap-2">
+      <div className="flex-1 text-center justify-start text-color-brand-primary text-base font-medium font-['Noto_Sans_KR'] leading-6">회원가입</div>
+    </div>
+  </div>
+  <div className="w-96 px-6 pt-3 pb-14 bg-color-bg-subtle flex flex-col justify-start items-start gap-2 overflow-hidden">
+    <div data-icon="None" data-size="L" data-state="Default" data-variant="Primary" className="self-stretch h-14 relative rounded-2xl inline-flex justify-center items-center gap-2">
+      <div className="w-80 h-14 left-0 top-0 absolute bg-color-brand-primary rounded-2xl" />
+      <div className="flex-1 text-center justify-start text-color-text-on-primary text-base font-medium font-['Noto_Sans_KR'] leading-6">로그인</div>
+    </div>
+  </div>
+  <div className="w-32 h-[5px] left-[129.50px] top-[830px] absolute bg-color-text-primary rounded-[999px]" />
+  <div className="w-28 h-8 left-[140px] top-[12px] absolute bg-black rounded-[999px]" />
+</div>
