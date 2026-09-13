03b3337a31cc6cb131a39c771583f0f500129d9e
Tacitus37 <lunaris37@naver.com>
2026-09-13 19:08:26 +0900

    Add signup component in signup.jsx

---
+++ move_agent_to_root/figma/prototype/a/signup.jsx
@@ -0,0 +1,46 @@
+<div className="w-96 h-[852px] relative bg-color-bg-subtle inline-flex flex-col justify-start items-start overflow-hidden">
+  <div className="w-96 h-14 px-6 bg-color-bg-subtle inline-flex justify-start items-center overflow-hidden">
+    <div className="flex-1 justify-start text-color-text-primary text-xs font-medium font-['Noto_Sans_KR'] leading-4">9:41</div>
+    <div className="flex-1 text-right justify-start text-color-text-primary text-xs font-medium font-['Noto_Sans_KR'] leading-4">▰</div>
+  </div>
+  <div className="w-96 px-6 flex flex-col justify-start items-start overflow-hidden">
+    <div className="self-stretch h-11 inline-flex justify-start items-center gap-2">
+      <div className="size-6 inline-flex flex-col justify-start items-start gap-2">
+        <div className="size-6 relative overflow-hidden">
+          <div className="w-1.5 h-3.5 left-[8px] top-[5px] absolute outline outline-2 outline-offset-[-0.90px] outline-slate-600" />
+        </div>
+      </div>
+      <div className="flex-1 justify-start text-color-text-primary text-base font-medium font-['Noto_Sans_KR'] leading-6">DOIT</div>
+      <div className="size-6 inline-flex flex-col justify-start items-start gap-2">
+        <div className="size-6 relative overflow-hidden">
+          <div className="size-4 left-[4px] top-[3px] absolute outline outline-2 outline-offset-[-0.90px] outline-slate-600" />
+        </div>
+      </div>
+    </div>
+  </div>
+  <div className="w-96 flex-1 px-6 pt-3 pb-6 flex flex-col justify-start items-start gap-4 overflow-hidden">
+    <div className="self-stretch justify-start text-color-brand-primary text-xs font-medium font-['Noto_Sans_KR'] leading-4">CREATE ACCOUNT</div>
+    <div className="self-stretch justify-start text-color-text-primary text-xl font-bold font-['Noto_Sans_KR'] leading-8">오늘부터, DOIT</div>
+    <div data-state="Default" className="self-stretch px-4 py-3 bg-color-bg-subtle rounded-xl outline outline-1 outline-offset-[-1px] outline-color-border-default flex flex-col justify-start items-start gap-2">
+      <div className="self-stretch justify-start text-color-text-secondary text-xs font-medium font-['Noto_Sans_KR'] leading-4">이메일</div>
+      <div className="self-stretch justify-start text-color-text-secondary text-base font-normal font-['Noto_Sans_KR'] leading-6">이메일을 입력해주세요</div>
+    </div>
+    <div data-state="Default" className="self-stretch px-4 py-3 bg-color-bg-subtle rounded-xl outline outline-1 outline-offset-[-1px] outline-color-border-default flex flex-col justify-start items-start gap-2">
+      <div className="self-stretch justify-start text-color-text-secondary text-xs font-medium font-['Noto_Sans_KR'] leading-4">비밀번호</div>
+      <div className="self-stretch justify-start text-color-text-secondary text-base font-normal font-['Noto_Sans_KR'] leading-6">8자 이상 입력해주세요</div>
+    </div>
+    <div data-state="Default" className="self-stretch px-4 py-3.5 bg-color-bg-default rounded-xl outline outline-1 outline-offset-[-1px] outline-color-border-default inline-flex justify-start items-center gap-2">
+      <div className="flex-1 justify-start text-color-text-primary text-base font-normal font-['Noto_Sans_KR'] leading-6">이용약관 및 개인정보 처리 안내 확인</div>
+      <div className="size-6 justify-start text-color-brand-primary text-base font-normal font-['Noto_Sans_KR'] leading-6">○</div>
+    </div>
+    <div className="self-stretch justify-start text-color-text-secondary text-base font-normal font-['Noto_Sans_KR'] leading-6">프로토타입에서는 개인정보를 저장하지 않아요.</div>
+  </div>
+  <div className="w-96 px-6 pt-3 pb-14 bg-color-bg-subtle flex flex-col justify-start items-start gap-2 overflow-hidden">
+    <div data-icon="None" data-size="L" data-state="Default" data-variant="Primary" className="self-stretch h-14 relative rounded-2xl inline-flex justify-center items-center gap-2">
+      <div className="w-80 h-14 left-0 top-0 absolute bg-color-brand-primary rounded-2xl" />
+      <div className="flex-1 text-center justify-start text-color-text-on-primary text-base font-medium font-['Noto_Sans_KR'] leading-6">DOIT 시작하기</div>
+    </div>
+  </div>
+  <div className="w-32 h-[5px] left-[129.50px] top-[830px] absolute bg-color-text-primary rounded-[999px]" />
+  <div className="w-28 h-8 left-[140px] top-[12px] absolute bg-black rounded-[999px]" />
+</div>
